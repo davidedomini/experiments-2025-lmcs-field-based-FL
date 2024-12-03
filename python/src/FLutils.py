87dadf0661eb0b3b24cb492e74247dc25e2bd6bb
Davide Domini <davide.domini@unibo.it>
2024-12-03 14:17:37 +0100

    feat: implement dataset partitioning given a certain data distribution #11

--- python/src/FLutils.py
+++ python/src/FLutils.py
@@ -26,3 +26,22 @@ def hard_non_iid_mapping(areas: int, labels: int) -> np.ndarray:
         rows = [i for _ in elems]
         distribution[rows, elems] = 1
     return distribution
+
+def partitioning(distribution: np.ndarray, dataset: Dataset) -> dict[int, list[int]]:
+    targets = dataset.targets
+    areas = distribution.shape[0]
+    targets_cardinality = distribution.shape[1]
+    class_counts = torch.bincount(targets)
+    partitions = {}
+    for area in range(areas):
+        area_distribution = distribution[area, :]
+        elements_per_class = torch.tensor(area_distribution) * class_counts
+        elements_per_class = torch.floor(elements_per_class).to(torch.int)
+        selected_indices = []
+        for label in range(targets_cardinality):
+            target_indices = torch.where(targets == label)[0]
+            selected_count = min(len(target_indices), elements_per_class[label].item())
+            if selected_count > 0:
+                selected_indices.extend(target_indices[:selected_count].tolist())
+        partitions[area] = selected_indices
+    return partitions
