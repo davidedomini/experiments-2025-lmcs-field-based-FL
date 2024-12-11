8a83ead42180a527722b179b645e801ebcf4ceba
davidedomini <davide.domini@unibo.it>
2024-12-11 12:30:06 +0100

    feat: implement client for centralized learning

---
+++ src/main/scala/it/unibo/scafi/CentralizedClient.scala
@@ -0,0 +1,45 @@
+package it.unibo.scafi
+
+import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
+import it.unibo.interop.PythonModules.flUtils
+import it.unibo.learning.model.Dataset
+import it.unibo.scafi.Utils.slices
+import me.shadaj.scalapy.py
+import me.shadaj.scalapy.py.{PyQuote, SeqConverters}
+
+class CentralizedClient extends
+  AggregateProgram
+  with BuildingBlocks
+  with StandardSensors
+  with ScafiAlchemistSupport {
+
+  private lazy val data = senseEnvData[Dataset](Molecules.phenomena)
+  private lazy val myIdInArea = sense[Int](Molecules.idInArea)
+  private lazy val neighbors = sense[Int](Molecules.neighbors)
+  private lazy val trainingDataInterval = slices(data.trainingData.size, neighbors)(myIdInArea)
+  private lazy val validationDataInterval = slices(data.validationData.size, neighbors)(myIdInArea)
+  private lazy val trainingDataIndexes = data.trainingData.slice(trainingDataInterval._1, trainingDataInterval._2)
+  private lazy val validationDataIndexes = data.validationData.slice(validationDataInterval._1, validationDataInterval._2)
+  private lazy val trainingData = flUtils.to_subset(data.dataset, trainingDataIndexes.toPythonProxy)
+  private lazy val validationData = flUtils.to_subset(data.dataset, validationDataIndexes.toPythonProxy)
+  private lazy val epochs = sense[Int](Molecules.epochs)
+  private lazy val batchSize = sense[Int](Molecules.batchSize)
+
+  override def main(): Any = {
+
+    val globalModel = sense[py.Dynamic](Molecules.globalModel)
+
+    val trainingResult = flUtils.training(globalModel, trainingData, validationData, epochs, batchSize)
+    val localModel = py"$trainingResult[0]"
+    val trainingLoss = py"$trainingResult[1]"
+    val validationLoss = py"$trainingResult[2]"
+    val validationAccuracy = py"$trainingResult[3]"
+
+    node.put(Molecules.trainingLoss, trainingLoss)
+    node.put(Molecules.validationLoss, validationLoss)
+    node.put(Molecules.validationAccuracy, validationAccuracy)
+
+    node.put(Molecules.localModel, localModel)
+  }
+
+}
--- src/main/scala/it/unibo/scafi/Molecules.scala
+++ src/main/scala/it/unibo/scafi/Molecules.scala
@@ -5,4 +5,13 @@ object Molecules {
   val phenomena = "Phenomena"
   val neighbors = "Neighbors"
   val idInArea = "IdInArea"
+  val globalModel = "GlobalModel"
+  val localModel = "LocalModel"
+  val epochs = "Epochs"
+  val batchSize = "BatchSize"
+  val seed = "Seed"
+  val trainingLoss = "TrainingLoss"
+  val validationLoss = "ValidationLoss"
+  val validationAccuracy = "ValidationAccuracy"
+
 }
