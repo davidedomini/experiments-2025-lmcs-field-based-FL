dfdd770bc6bbb1f6d0904a73e5163dfcdf0b55f5
Davide Domini <davide.domini@unibo.it>
2024-12-12 14:44:46 +0100

    feat: implement reaction to init the global model

---
+++ src/main/scala/it/unibo/alchemist/model/implementations/reactions/ModelAndSeedInitialization.scala
@@ -0,0 +1,26 @@
+package it.unibo.alchemist.model.implementations.reactions
+
+import it.unibo.alchemist.model.{Environment, Position, TimeDistribution}
+import it.unibo.alchemist.model.molecules.SimpleMolecule
+import it.unibo.interop.PythonModules._
+import it.unibo.scafi.Molecules
+
+
+class ModelAndSeedInitialization[T, P <: Position[P]](
+ environment: Environment[T, P],
+ timeDistribution: TimeDistribution[T],
+ seed: Int,
+ experiment: String
+) extends AbstractGlobalReaction(environment, timeDistribution)  {
+
+  override protected def executeBeforeUpdateDistribution(): Unit = {
+    flUtils.seed_everything(seed)
+    val model = flUtils
+      .instantiate_model(null, experiment, false)
+      .state_dict()
+    println(model)
+    nodes.foreach { node =>
+      node.setConcentration(new SimpleMolecule(Molecules.globalModel), model.asInstanceOf[T])
+    }
+  }
+}
